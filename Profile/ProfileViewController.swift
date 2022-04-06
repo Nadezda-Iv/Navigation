8fd7fd27a1d11ba2163fce1f8dc1a27e46083de3
Nadezda-Iv <nadezda.a.k@yandex.ru>
2022-04-06 16:23:39 +0300

    Домашнее задание по анимации

--- Profile/ProfileViewController.swift
+++ Profile/ProfileViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-
+    
     
     private var tap = UITapGestureRecognizer()
     
@@ -22,7 +22,7 @@ class ProfileViewController: UIViewController {
         return label
     }()
     
-     lazy var statusLabel: UILabel = {
+    lazy var statusLabel: UILabel = {
         let label = UILabel()
         label.text = "Smile"
         label.font = UIFont.systemFont(ofSize: 14.0)
@@ -31,16 +31,16 @@ class ProfileViewController: UIViewController {
         return label
     }()
     
-     lazy var textField: UITextField = {
+    lazy var textField: UITextField = {
         let textField = UITextField()
-        //textField.isHidden = false
+        textField.isHidden = false
         textField.backgroundColor = .black
         textField.translatesAutoresizingMaskIntoConstraints = false
         textField.layer.cornerRadius = 12
         return textField
     }()
     
-     lazy var statusButton: UIButton = {
+    lazy var statusButton: UIButton = {
         let button = UIButton()
         button.backgroundColor = .systemBlue
         button.layer.cornerRadius = 4
@@ -54,36 +54,28 @@ class ProfileViewController: UIViewController {
         stackView.axis = .vertical
         stackView.distribution = .fillEqually
         stackView.spacing = 5
-      
+        
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
     
-    private lazy var infoStackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.axis = .horizontal
-        stackView.spacing = 10
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        return stackView
-    }()
     
     private lazy var exitButton: UIButton = {
-         let button = UIButton()
-         button.layer.cornerRadius = 20
-         button.alpha = 1
-         button.clipsToBounds = true
-         button.setBackgroundImage(UIImage(named: "img_230392"), for: .normal)
-         //button.addTarget(self, action: #selector(self.didTapSetStatusButton), for: .touchUpInside)
-         button.translatesAutoresizingMaskIntoConstraints = false
-         return button
-     }()
+        let button = UIButton()
+        button.layer.cornerRadius = 20
+        button.alpha = 0
+        button.clipsToBounds = true
+        button.setBackgroundImage(UIImage(named: "img_230392"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
     
-
-        lazy var imageview: UIImageView = {
+    
+    lazy var imageview: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(named: "avatar")
         imageView.backgroundColor = .systemRed
-       
+        
         imageView.layer.borderWidth = 3
         imageView.frame.size = CGSize(width: 80, height: 80)
         imageView.layer.cornerRadius = 50
@@ -108,11 +100,11 @@ class ProfileViewController: UIViewController {
     
     private lazy var changeTitleButton: UIButton = {
         let button = UIButton()
-         button.backgroundColor = .systemBlue
-         button.setTitle("Change title", for: .normal)
-         button.translatesAutoresizingMaskIntoConstraints = false
-         return button
-      }()
+        button.backgroundColor = .systemBlue
+        button.setTitle("Change title", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
     
     private lazy var tableView: UITableView = {
         let tableView = UITableView()
@@ -146,17 +138,16 @@ class ProfileViewController: UIViewController {
     
     open override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
-       
-       statusButton.addTarget(self, action: #selector(printProfileState), for: .touchUpInside)
+        
+        statusButton.addTarget(self, action: #selector(printProfileState), for: .touchUpInside)
         textField.addTarget(self, action: #selector(changeProfileState), for: .editingChanged)
         self.changeTitleButton.addTarget(self, action: #selector(changingTitle), for: .touchUpInside)
-        
-        //view.addSubview(profileHeaderView)
-       // self.profileHeaderView = profileHeaderView
+        exitButton.addTarget(self, action: #selector(exitAnimationAvatar), for: .touchUpInside)
     }
     
     private var heightConstraint: NSLayoutConstraint?
-
+    private var alphaBottomConstraint: NSLayoutConstraint?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setupView()
@@ -166,7 +157,7 @@ class ProfileViewController: UIViewController {
             self?.tableView.reloadData()
         }
     }
-
+    
     private func setupView() {
         
         
@@ -176,21 +167,23 @@ class ProfileViewController: UIViewController {
         
         self.view.addSubview(self.changeTitleButton)
         self.view.addSubview(self.tableView)
+        self.view.addSubview(self.imageview)
+        self.view.addSubview(self.exitButton)
         
         self.view.addSubview(self.alphaForTableView)
         
-        self.alphaForTableView.addSubview(infoStackView)
-        self.labelsStackView.addSubview(nameLabel)
-        self.labelsStackView.addSubview(statusLabel)
-        self.infoStackView.addSubview(labelsStackView)
-        self.alphaForTableView.addSubview(self.imageview)
-        self.alphaForTableView.addSubview(self.exitButton)
+        self.alphaForTableView.addSubview(labelsStackView)
         self.alphaForTableView.addSubview(statusButton)
         self.alphaForTableView.addSubview(textField)
         
-       
+        self.labelsStackView.addSubview(nameLabel)
+        self.labelsStackView.addSubview(statusLabel)
+        
+        
         self.alphaForTableView.alpha = 1
-        self.view.bringSubviewToFront(self.alphaForTableView)
+        self.view.bringSubviewToFront(self.imageview)
+        self.view.bringSubviewToFront(self.exitButton)
+        
         
         
         self.imageViewTopConstraint = imageview.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 16)
@@ -200,26 +193,31 @@ class ProfileViewController: UIViewController {
         let alphaTop = self.alphaForTableView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor)
         let alphsLeading = self.alphaForTableView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor)
         let alphaTrailing = self.alphaForTableView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor)
-        let alphaBottom = self.alphaForTableView.heightAnchor.constraint(equalToConstant: 220)
+        alphaBottomConstraint = self.alphaForTableView.heightAnchor.constraint(equalToConstant: 220)
         
         
-        let infoStackTopConstraint = infoStackView.topAnchor.constraint(equalTo: self.alphaForTableView.topAnchor, constant: 16)
-        let infoStackLeftConstraint = infoStackView.leftAnchor.constraint(equalTo: self.alphaForTableView.leftAnchor, constant: 150)
-        let infoStackRight = infoStackView.rightAnchor.constraint(equalTo: self.alphaForTableView.rightAnchor, constant: -16)
-        let infoStackHeight = infoStackView.heightAnchor.constraint(equalToConstant: 150)
-
+        
+        let labelStackTopConstraint = labelsStackView.topAnchor.constraint(equalTo: alphaForTableView.topAnchor, constant: 16)
+        let labelStackLeftConstraint = labelsStackView.leftAnchor.constraint(equalTo: alphaForTableView.leftAnchor, constant: 150)
+        let labelStackRightConstraint = labelsStackView.rightAnchor.constraint(equalTo: alphaForTableView.rightAnchor, constant: -16)
+        let labelStackBottomConstraint = labelsStackView.heightAnchor.constraint(equalToConstant: 150)
+        
+        let labelTop = statusLabel.topAnchor.constraint(equalTo: labelsStackView.topAnchor, constant: 25)
+        let labelLeft = statusLabel.leftAnchor.constraint(equalTo: labelsStackView.leftAnchor)
+        let labelRight = statusLabel.rightAnchor.constraint(equalTo: labelsStackView.rightAnchor, constant: -5)
+        let labelHeight = statusLabel.heightAnchor.constraint(equalToConstant: 50)
         
         let statusButtonLeftConstraint = statusButton.leftAnchor.constraint(equalTo: self.alphaForTableView.leftAnchor, constant: 16)
         let statusButtonRightConstraint = statusButton.rightAnchor.constraint(equalTo: self.alphaForTableView.rightAnchor, constant: -16)
-        let statusButtonTop = statusButton.topAnchor.constraint(equalTo: self.infoStackView.bottomAnchor, constant: 20)
+        let statusButtonTop = statusButton.topAnchor.constraint(equalTo: self.labelsStackView.bottomAnchor, constant: 20)
         let statusButtonHeight = statusButton.heightAnchor.constraint(equalToConstant: 50)
         
-        let textFieldRight = textField.leftAnchor.constraint(equalTo: infoStackView.rightAnchor, constant: -15)
-        let textFieldLeft = self.textField.leftAnchor.constraint(equalTo: infoStackView.leftAnchor, constant: 250)
-        let textFieldTop = self.textField.topAnchor.constraint(equalTo: infoStackView.bottomAnchor, constant: 10)
+        let textFieldRight = textField.leftAnchor.constraint(equalTo: labelsStackView.rightAnchor, constant: -15)
+        let textFieldLeft = self.textField.leftAnchor.constraint(equalTo: labelsStackView.leftAnchor, constant: 250)
+        let textFieldTop = self.textField.topAnchor.constraint(equalTo: labelsStackView.bottomAnchor, constant: 10)
         let textFieldHeight = self.textField.heightAnchor.constraint(equalToConstant: 40)
         
-
+        
         
         let topExitbutton = self.exitButton.topAnchor.constraint(equalTo: self.alphaForTableView.topAnchor, constant: 25)
         let rightExitButton = self.exitButton.rightAnchor.constraint(equalTo: self.alphaForTableView.rightAnchor, constant: -25)
@@ -228,7 +226,7 @@ class ProfileViewController: UIViewController {
         
         let avatarTop = imageview.widthAnchor.constraint(equalToConstant: 80)
         let avatarHeight = imageview.heightAnchor.constraint(equalToConstant: 80)
-      
+        
         
         let tableViewTopConstraint = self.tableView.topAnchor.constraint(equalTo: self.alphaForTableView.bottomAnchor)
         let tableViewLeadingConstraint = self.tableView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16)
@@ -239,17 +237,17 @@ class ProfileViewController: UIViewController {
         let changeButtonLeadingConstraint = self.changeTitleButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor)
         let changeButtonTrailingConstraint = self.changeTitleButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor)
         let changeButtonBottomConstraint = self.changeTitleButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor)
-            
+        
         
         NSLayoutConstraint.activate([
-     
+            
             tableViewTopConstraint, tableViewLeadingConstraint, tableViewTrailingConstraint, tableViewBottomConstraint, changeButtonBottomConstraint, changeButtonHeightConstraint, changeButtonLeadingConstraint, changeButtonTrailingConstraint,
-                alphaTop, alphaBottom, alphsLeading, alphaTrailing,
+            alphaTop, alphaBottomConstraint, alphsLeading, alphaTrailing,
             topExitbutton, rightExitButton, wightExitButton, heightExitButton,
             avatarTop, avatarHeight, self.imageViewTopConstraint, self.imageViewLeftConstraint,
-            infoStackTopConstraint, infoStackRight, infoStackLeftConstraint, infoStackHeight,
             statusButtonTop, statusButtonHeight, statusButtonLeftConstraint, statusButtonRightConstraint,
-            textFieldTop, textFieldLeft, textFieldRight, textFieldHeight
+            textFieldTop, textFieldLeft, textFieldRight, textFieldHeight, labelStackTopConstraint, labelStackLeftConstraint, labelStackRightConstraint, labelStackBottomConstraint,
+            labelTop, labelLeft, labelRight, labelHeight
         ].compactMap({ $0 }))
         
     }
@@ -295,51 +293,69 @@ class ProfileViewController: UIViewController {
         }
         self.navigationController?.pushViewController(photoVC, animated: true)
     }
-
+    
     private func setupGesture() {
         self.tap.addTarget(self, action: #selector(self.handleTap(_ :)))
         self.imageview.addGestureRecognizer(self.tap)
+        
         print("tap")
     }
-
+    
     @objc func handleTap(_ gesture: UITapGestureRecognizer) {
-      let tapLocation = gesture.location(in: imageview.superview)
-      if (imageview.layer.presentation()?.frame.contains(tapLocation))! {
-        print("Bug tapped!")
-        UIView.animate(withDuration: 0.7, delay: 0.0, options: [.curveEaseOut , .beginFromCurrentState], animations: {
-            self.exitButton.alpha = 1
-            self.imageViewTopConstraint?.isActive = false
-            self.imageViewLeftConstraint?.isActive = false
-            self.imageViewTopConstraint = self.imageview.centerXAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerXAnchor)
-            self.imageViewLeftConstraint = self.imageview.centerYAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerYAnchor)
-            NSLayoutConstraint.activate([
-                self.imageViewLeftConstraint, self.imageViewTopConstraint
-            ].compactMap({ $0 }))
-            
-          self.imageview.transform = CGAffineTransform(scaleX: 5, y: 5)
-        }, completion: { finished in
-          UIView.animate(withDuration: 2.0, delay: 2.0, options: [], animations: {
-            //self.imageview.alpha = 0.0
-          }, completion: { finished in
-              self.imageview.transform = CGAffineTransform(scaleX: 1, y: 1)
-            //self.imageview.removeFromSuperview()
-          })
-        })
-      } else {
-        print("Bug not tapped!")
-      }
+        let tapLocation = gesture.location(in: imageview.superview)
+        if (imageview.layer.presentation()?.frame.contains(tapLocation))! {
+            UIView.animate(withDuration: 0.5, delay: 0.0, options: [.curveEaseOut , .beginFromCurrentState], animations: {
+                self.alphaForTableView.alpha = 0.5
+                self.imageViewTopConstraint?.isActive = false
+                self.imageViewLeftConstraint?.isActive = false
+                self.alphaBottomConstraint?.isActive = false
+                self.imageViewTopConstraint = self.imageview.centerXAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerXAnchor)
+                self.imageViewLeftConstraint = self.imageview.centerYAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerYAnchor)
+                self.alphaBottomConstraint = self.alphaForTableView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor)
+                NSLayoutConstraint.activate([
+                    self.imageViewLeftConstraint, self.imageViewTopConstraint, self.alphaBottomConstraint
+                ].compactMap({ $0 }))
+                let screen = UIScreen.main.bounds
+                let screenWidth = screen.size.width / 80
+                self.imageview.transform = CGAffineTransform(scaleX: screenWidth, y: screenWidth)
+                UIView.animate(withDuration: 0.3, delay: 0.5) {
+                    self.exitButton.alpha = 1
+                }
+            })
+        } else {
+            print("not tapped!")
+        }
     }
     
-
-    
+    @objc private func exitAnimationAvatar(sender: UIButton!) {
+        UIView.animate(withDuration: 0.3, delay: 0.5) {
+            self.exitButton.alpha = 0
+        }
+        
+        UIView.animate(withDuration: 0.5, delay: 0.0, options: [.curveEaseOut , .beginFromCurrentState], animations: {
+            self.imageViewTopConstraint?.isActive = false
+            self.imageViewLeftConstraint?.isActive = false
+            self.alphaBottomConstraint?.isActive = false
+            self.imageViewTopConstraint = self.imageview.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 16)
+            self.imageViewLeftConstraint = self.imageview.leftAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leftAnchor, constant: 16)
+            self.alphaBottomConstraint = self.alphaForTableView.heightAnchor.constraint(equalToConstant: 220)
+            NSLayoutConstraint.activate([
+                self.imageViewLeftConstraint, self.imageViewTopConstraint, self.alphaBottomConstraint
+            ].compactMap({ $0 }))
+            self.exitButton.alpha = 0
+            self.alphaForTableView.alpha = 1
+            self.imageview.transform = CGAffineTransform(scaleX: 1, y: 1)
+        })
+    }
 }
+
 
 extension ProfileViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return dataSource.count + 1
-        }
-
+    }
+    
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if indexPath.row == 0 {
@@ -362,7 +378,7 @@ extension ProfileViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView( _ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.row == 0 {
-        self.navigationController?.pushViewController(PhotosViewController(), animated: true)
+            self.navigationController?.pushViewController(PhotosViewController(), animated: true)
         } else { return }
     }
 }
